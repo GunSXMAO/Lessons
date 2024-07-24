654ee207cf78204c2fd6a424452921009216de59
GunSXMAO <arminbuuren86@gmail.com>
2024-07-24 23:09:25 +0500

    Create Homework2.py

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+Number_of_completed = 12
+Number_hours_spent = 1.5
+Course_name = "Python"
+Time_one_task = Number_hours_spent / Number_of_completed
+print("Курс: " + Course_name + ", всего задач: " + str(Number_of_completed) + ", затрачено часов: " + str(Number_hours_spent) + ", среднее время выполнения " + str(Time_one_task) + " часа.")
