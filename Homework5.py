ae1405f0e8875e2a9f40c4751ec51ed97fcdd12b
GunSXMAO <arminbuuren86@gmail.com>
2024-07-26 20:22:17 +0500

    Update Homework5.py

--- Homework5.py
+++ Homework5.py
@@ -1,1 +1,16 @@
-
+immutable_var = (1, "string", True, 1.5, [1, 2, 3])
+print(immutable_var)
+#immutable_var[0] = 2 - произойдет ошибка, числа не изменяются
+#immutable_var[1] = "apple" - произойдет ошибка, строки не изменяются
+#immutable_var[2] = False - произойдет ошибка, логические данные не изменяются
+#immutable_var[3] = 2.3 - произойдет ошибка, числа(дробные числа) не изменяются
+immutable_var[4][0] = 4 # - список внутри кортежа, можно изменить любой элемент.
+print(immutable_var)
+print()
+mutable_list = [1, "string", True, 1.5]
+print("Было: ", mutable_list)
+mutable_list[0] = "string"
+mutable_list[1] = 1
+mutable_list[2] = 1.5
+mutable_list[3] = False
+print("Стало: ", mutable_list)
