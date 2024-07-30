d047297057fa6fc0f5ac266d9674a4f367fcfa8a
GunSXMAO <arminbuuren86@gmail.com>
2024-07-30 21:19:45 +0500

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,3 +1,4 @@
+# Первый вариант.
 my_list = [42, 2, 0, 13, 0, 99, -3, 9, 8, 7, 1, 5]
 len_list = len(my_list)
 counting = 0
@@ -12,3 +13,18 @@ while True:
             print(new)
         continue
     break
+
+
+# Второй вариант.
+my_list = [42, 2, 1, 13, 0, 99, -3, 9, 8, 7, 1, 5]
+index = 0
+while index < len(my_list):
+    if my_list[index] == 0:
+        index += 1
+        continue
+    elif my_list[index] <= -1:
+        break
+    elif my_list[index] >= 1:
+        print(my_list[index])
+        index += 1
+        continue
