7be4160f7a4b6642c72ec985e7690ad44977c438
GunSXMAO <arminbuuren86@gmail.com>
2024-08-07 21:58:37 +0500

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,15 +1,17 @@
-n = int(input("Введите первое число: "))
-result = []
-for j in range(1, 21):
-    for k in range(1, 21):
-        i = j + k
-        if j == k:
-            continue
-        if n % i == 0:
-            result.append(j)
-            result.append(k)
-lst = len(result)//2
-for i in range(lst):
-    result.pop()
-for x in result:
-   print(x, end='')
+target_number = int(input("Введите число от 3 до 20: "))
+numbers_range = range(1, 21)  # числа от 1 до 20
+
+# Находим все уникальные пары чисел, сумма которых кратна target_number
+valid_pairs = []
+for i in numbers_range:
+    for j in numbers_range:
+        if i != j:
+            if target_number % (i + j) == 0:
+                pair = (i, j)
+                if pair not in valid_pairs and (j, i) not in valid_pairs:
+                    valid_pairs.append(pair)
+
+# Выводим найденные уникальные пары
+print(f"Уникальные пары чисел от 1 до 20, сумма которых кратна {target_number}:")
+
+print(valid_pairs)
