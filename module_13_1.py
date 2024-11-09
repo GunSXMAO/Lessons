025a792bee9dc4d9cf1e6150db0c0cd7291d0430
GunSXMAO <arminbuuren86@gmail.com>
2024-11-09 20:18:07 +0500

    Create module_13_1.py

---
+++ module_13_1.py
@@ -0,0 +1,18 @@
+import asyncio
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования.')
+    for i in range(5):
+        print(f'Силач {name} поднял {power}')
+        await asyncio.sleep(power)
+    print(f'Силач {name} закончил соревнование.')
+
+async def start_tournament():
+    one = asyncio.create_task(start_strongman('Pasha', 3))
+    two = asyncio.create_task(start_strongman('Denis', 4))
+    three = asyncio.create_task(start_strongman('Apollon', 5))
+    await one
+    await two
+    await three
+
+asyncio.run(start_tournament())
